066f33f6ae84a4cf596fda8e0ff0203720cef725
takito1812 <56491288+takito1812@users.noreply.github.com>
2020-12-19 17:04:35 +0100

    Update herobrine.sh

--- herobrine.sh
+++ herobrine.sh
@@ -38,11 +38,11 @@ function printBanner {
 function initialSetup {
     echo -e "${yellowColor}[*]Changing hostname to "herobrine"...${resetColor}"
     if ! grep -q "herobrine" /etc/hostname; then
-        echo "herobrine" >>/etc/hostname
+        echo "herobrine" >/etc/hostname
     fi
 
     if ! grep -q "herobrine" /etc/hosts; then
-        sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/g' /etc/hosts
+        sed -i 's/raspberrypi/herobrine/g' /etc/hosts
     fi
 
     echo -e "${yellowColor}[*] Updating list of repositories...${resetColor}"
